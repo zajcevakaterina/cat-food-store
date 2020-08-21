42abb7212f72488bf111c3e3e0cb293e854d7d1a
Ekaterina Zaytseva <zajceva.katerina@gmail.com>
2020-08-21 12:33:03 +0300

    Updates

---
+++ precache-manifest.5f3c7f77514f67cb7c31d3ba4d58074f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28363b539f6e88701a109f8b74e6dc98",
+    "url": "/cat-food-store/index.html"
+  },
+  {
+    "revision": "97aab8bf7adddfc0d977",
+    "url": "/cat-food-store/static/css/main.37b187cf.chunk.css"
+  },
+  {
+    "revision": "9901f80ef95d3f0de209",
+    "url": "/cat-food-store/static/js/2.999e67b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-food-store/static/js/2.999e67b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97aab8bf7adddfc0d977",
+    "url": "/cat-food-store/static/js/main.88547ea4.chunk.js"
+  },
+  {
+    "revision": "da6fa8fa76d2685c13e8",
+    "url": "/cat-food-store/static/js/runtime-main.6fd0a521.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/cat-food-store/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "63f3ee9c747c86d1643376b970373180",
+    "url": "/cat-food-store/static/media/cat-image.63f3ee9c.svg"
+  },
+  {
+    "revision": "7ea1b7b633f40d8b383c7e618cb02a8a",
+    "url": "/cat-food-store/static/media/pattern.7ea1b7b6.svg"
+  }
+]);
